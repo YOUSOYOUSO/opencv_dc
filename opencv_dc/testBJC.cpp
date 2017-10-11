1128d01a2fa3efcf5d778572ecdc19705c9bf31b
d p <980853230@qq.com>
2017-10-11 10:16:37 +0800

    更改了一下测试图大小300*200

--- opencv_dc/testBJC.cpp
+++ opencv_dc/testBJC.cpp
@@ -8,36 +8,35 @@ using namespace cv;
 
 int testBJC()
 {
-	Mat img = imread("1.png");
 	Mat test1 = imread("test1.png");
 	Mat background;
 	Mat temp;
 	Mat result;
-	resize(img, img, Size(300, 300));//resize为300*300的图像
-	cvtColor(img, img, CV_RGB2GRAY);//转为灰度图
-	imshow("grey1.png", img);
-
-	//遍历图像，二值化，阈值为50
-	for (int i = 0; i < img.rows; i++){
-		for (int j = 0; j < img.cols; j++){
-			if(img.at<uchar>(i, j)<50){
-				img.at<uchar>(i, j) = 0;
-			}
-			else {
-				img.at<uchar>(i, j) = 255;
-			}
-			
-		}
-	}
-	imshow("gray_result", img);
-
 	VideoCapture capture("7.mp4");
 	capture.read(background);
-	absdiff(background,test1,result);
+
+	resize(background, background, Size(300, 200));//resize为300*200的图像，原来是720*480
+	cvtColor(background, background, CV_RGB2GRAY);//转为灰度图
+
+	resize(test1, test1, Size(300, 200));//resize为300*200的图像，原来是720*480
+	cvtColor(test1, test1, CV_RGB2GRAY);//转为灰度图
+
+	absdiff(background, test1, result);//和背景作差
+
+	//遍历图像，二值化，阈值为50
+	for (int i = 0; i < result.rows; i++) {
+		for (int j = 0; j < result.cols; j++) {
+			if (result.at<uchar>(i, j)<50) {
+				result.at<uchar>(i, j) = 0;
+			}
+			else {
+				result.at<uchar>(i, j) = 255;
+			}
+
+		}
+	}
 	imshow("result",result);
-	//Mat diff;
-	//diff = absdiff(test1,ob，);
-	//imshow("diff", diff);
+
 	waitKey(0);
 	return 0;
 }
