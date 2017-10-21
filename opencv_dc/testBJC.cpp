8bd4cbaa147475fcefb7613287a9466f1daa6ec9
d p <980853230@qq.com>
2017-10-21 14:25:04 +0800

    添加完成mat类的背景差法，包括背景更新和像素统计，需要再做一个写入excel用于统计的writeexcel.cpp

--- opencv_dc/testBJC.cpp
+++ opencv_dc/testBJC.cpp
@@ -12,31 +12,48 @@ int testBJC()
 	Mat background;
 	Mat temp;
 	Mat result;
-	VideoCapture capture("7.mp4");
-	capture.read(background);
+	
+	int FrameCount = 0;
+	VideoCapture capture("2.mp4");
 
-	resize(background, background, Size(300, 200));//resize为300*200的图像，原来是720*480
-	cvtColor(background, background, CV_RGB2GRAY);//转为灰度图
-
-	resize(test1, test1, Size(300, 200));//resize为300*200的图像，原来是720*480
-	cvtColor(test1, test1, CV_RGB2GRAY);//转为灰度图
-
-	absdiff(background, test1, result);//和背景作差
-
-	//遍历图像，二值化，阈值为50
-	for (int i = 0; i < result.rows; i++) {
-		for (int j = 0; j < result.cols; j++) {
-			if (result.at<uchar>(i, j)<50) {
-				result.at<uchar>(i, j) = 0;
-			}
-			else {
-				result.at<uchar>(i, j) = 255;
-			}
-
-		}
+	if (!capture.isOpened())
+	{
+		cout << "读取视频有误" << endl;
+		return -1;
 	}
-	imshow("result",result);
+	else
+	{
+		capture.read(background);
+		resize(background, background, Size(300, 200));//resize为300*200的图像，原来是720*480
+		cvtColor(background, background, CV_RGB2GRAY);//转为灰度图
+	}
+	while (capture.read(temp))
+	{
+		int PixelCount = 0;
+		FrameCount++;
+		resize(temp, temp, Size(300, 200));//resize为300*200的图像，原来是720*480
+		cvtColor(temp, temp, CV_RGB2GRAY);//转为灰度图
+		absdiff(background, temp, result);//和背景作差,输出result
+		addWeighted(background, 0.95, temp, 1-0.95, 0, background);
 
+		//遍历图像，二值化，阈值为50
+		for (int i = 0; i < result.rows; i++) {
+			for (int j = 0; j < result.cols; j++) {
+				if (result.at<uchar>(i, j)<50) {
+					result.at<uchar>(i, j) = 0;
+				}
+				else {
+					PixelCount++;
+					result.at<uchar>(i, j) = 255;
+				}
+			}
+		}
+		cout <<FrameCount<< "当前背景有" << PixelCount << "个前景像素"<<endl;
+		imshow("当前视频",result);
+		imshow("背景图片",background);
+		waitKey(50);
+	}
 	waitKey(0);
+
 	return 0;
 }
