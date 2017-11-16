2e58e9503f215a2245f672a9cc3cce2e760cc5e8
d p <980853230@qq.com>
2017-11-16 12:17:33 +0800

    增加excel

--- opencv_dc/readImg.cpp
+++ opencv_dc/readImg.cpp
@@ -2,51 +2,20 @@
 #include "opencv2/opencv.hpp" 
 #include <opencv2/core/core.hpp>  
 #include <opencv2/highgui/highgui.hpp>  
-using namespace std; using namespace cv;
+using namespace std; 
+using namespace cv;
 
 int readImg()
 {
 	//读入图像，并将之转为单通道图像 
-	Mat im = imread("1.png");
-	Mat im1;
-	if (im.empty())
+	Mat im1 = imread("1.png", 0);
+	if (im1.empty())
 	{
-		cout << "Can not load image." << endl;
-		return -1;
+		cout << "Can not load image." << endl;        
+			return -1;
 	}
-	//显示图片
-	namedWindow("1");		
-	imshow("1", im);
-	cvtColor(im, im1, CV_BGR2GRAY);
-	namedWindow("2");
-	imshow("2", im1);
-	//设置阈值
-	threshold(im1, im1, 255 * (0.5), 255, THRESH_BINARY);
-	// 形态学操作
-	// 如果把结构元素设置为Mat()，则将用默认的3*3的矩形结构元素
-	Mat eroded;
-    erode(im1, eroded, Mat());
-	Mat dilated;
-    dilate(im1, dilated, Mat());
-	// 定义结构元素
-	Mat closed, opened;
-	Mat se(5, 5, CV_8U, Scalar(1));
-	morphologyEx(im1, closed, MORPH_CLOSE, se);
-	morphologyEx(im1, opened, MORPH_OPEN, se);
-	namedWindow("xintaixue");
-	imshow("xintaixue", im1);
-	//等待两秒
+	namedWindow("1");		// 在窗口中显示游戏原画    
+	imshow("1", im1);			// 等待6000 ms后窗口自动关闭    
 	waitKey(2000);
-
-	Mat result;
-	//进行Canny操作，并将结果存于result 
-	Canny(im1, result, 50, 150);
-	//显示图片
-	namedWindow("3");
-	imshow("3", result);
-	// 等待两秒后窗口自动关闭    
-	waitKey(5000);
-	//写入图片
-	imwrite("2.png", result);
 	return 0;
 }
--- opencv_dc/testBJC.cpp
+++ opencv_dc/testBJC.cpp
@@ -2,6 +2,9 @@
 #include <opencv2\imgproc\imgproc.hpp>
 #include <opencv2\highgui\highgui.hpp>
 #include <iostream>
+#include<vector>
+#include <fstream>//fstream供给了三个类，用来实现c++对文件的操纵
+#include <iomanip>
 
 using namespace std;
 using namespace cv;
@@ -12,7 +15,7 @@ int testBJC()
 	Mat background;
 	Mat temp;
 	Mat result;
-	
+
 	int FrameCount = 0;
 	VideoCapture capture("2.mp4");
 
@@ -23,37 +26,51 @@ int testBJC()
 	}
 	else
 	{
+		long totalFrameNumber = capture.get(CV_CAP_PROP_FRAME_COUNT);//获取视频的总帧数
 		capture.read(background);
 		resize(background, background, Size(300, 200));//resize为300*200的图像，原来是720*480
 		cvtColor(background, background, CV_RGB2GRAY);//转为灰度图
-	}
-	while (capture.read(temp))
-	{
-		int PixelCount = 0;
-		FrameCount++;
-		resize(temp, temp, Size(300, 200));//resize为300*200的图像，原来是720*480
-		cvtColor(temp, temp, CV_RGB2GRAY);//转为灰度图
-		absdiff(background, temp, result);//和背景作差,输出result
-		addWeighted(background, 0.95, temp, 1-0.95, 0, background);
 
-		//遍历图像，二值化，阈值为50
-		for (int i = 0; i < result.rows; i++) {
-			for (int j = 0; j < result.cols; j++) {
-				if (result.at<uchar>(i, j)<50) {
-					result.at<uchar>(i, j) = 0;
-				}
-				else {
-					PixelCount++;
-					result.at<uchar>(i, j) = 255;
+		ofstream File;//ofstream类实现向文件写内容
+		File.open("test.xls", ios::_Noreplace|fstream::trunc);//ios::_Noreplace 打开一个文件时，如果文件不存在，创建该文件
+		vector<int>ivec(totalFrameNumber);//可变数组ivec，大小为总帧
+
+		while (capture.read(temp))
+		{
+
+			int PixelCount = 0;
+
+			FrameCount++;
+			resize(temp, temp, Size(300, 200));//resize为300*200的图像，原来是720*480
+			cvtColor(temp, temp, CV_RGB2GRAY);//转为灰度图
+			absdiff(background, temp, result);//和背景作差,输出result
+			addWeighted(background, 0.98, temp, 1 - 0.98, 0, background);
+
+			//遍历图像，二值化，阈值为50
+			for (int i = 0; i < result.rows; i++) {
+				for (int j = 0; j < result.cols; j++) {
+					if (result.at<uchar>(i, j) < 50) {
+						result.at<uchar>(i, j) = 0;
+					}
+					else {
+						PixelCount++;
+						result.at<uchar>(i, j) = 255;
+					}
 				}
 			}
-		}
-		cout <<FrameCount<< "当前背景有" << PixelCount << "个前景像素"<<endl;
-		imshow("当前视频",result);
-		imshow("背景图片",background);
-		waitKey(50);
-	}
-	waitKey(0);
 
+			cout << "整个视频共" << totalFrameNumber << "帧" << ","
+				<< "当前是第" << FrameCount << "帧" << ","
+				<< "当前背景有" << PixelCount << "个前景像素" << endl;
+
+			imshow("当前视频", result);
+			imshow("背景图片", background);
+			waitKey(10);
+
+			File << PixelCount << endl;	//写入excel
+
+		}
+		File.close();//释放
+	}
 	return 0;
 }
