5aac6cea5c621d035b0e9ecf3e3721e73f7b7d0a
d p <980853230@qq.com>
2017-11-18 21:05:43 +0800

    灰度视频写入失败

--- opencv_dc/SaveVideo.cpp
+++ opencv_dc/SaveVideo.cpp
@@ -7,18 +7,32 @@ using namespace std;
 int saveVideo()
 {
 	VideoCapture capture("2.avi");
-	VideoWriter writer("VideoTest.avi", CV_FOURCC('M', 'J', 'P', 'G'), 25.0, Size(300, 200));
+	int rate = capture.get(CV_CAP_PROP_FPS);
+	int delay = 1000 / rate;
+	VideoWriter writer("VideoTest.avi", CV_FOURCC('M', 'J', 'P', 'G'), rate, Size(300, 200),0);
 	Mat frame;
+	Mat background;
+	Mat result;
+	Mat mv[3];
+	capture.read(background);
+	resize(background, background, Size(300, 200));//resize为300*200的图像，原来是720*480
+	cvtColor(background, background, CV_RGB2GRAY);//转为灰度图
+	
 	long totalFrameNumber = capture.get(CV_CAP_PROP_FRAME_COUNT);//获取视频的总帧数
 	long frameNumber = 1.0;
 	while (capture.read(frame))
 	{
+		
 		resize(frame, frame, Size(300, 200));//resize为300*200的图像，原来是720*480
-		writer << frame;
-		if (cvWaitKey(20) == 27)
-		{
-			break;
-		}
+		cvtColor(frame, frame, CV_RGB2GRAY);//转为灰度图
+
+		split(frame, mv);
+		writer << mv[0];
+
+		cvNamedWindow("123123");
+		imshow("123123", mv[0]);
+		waitKey(delay);
+
 		frameNumber++;
 		cout << totalFrameNumber - frameNumber << endl;
 	}
